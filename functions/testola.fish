99500716c07ee3d62505466b5ffaf0d1b9bbe635
Alex Rakoczy <alexrakoczy@Alex-Rakoczys-Gigantic-Megacomputer.local>
2010-02-24 11:49:50 -0600

    testola uses dotime now

--- functions/testola.fish
+++ functions/testola.fish
@@ -1,12 +1,4 @@
 function testola
-	set start (date)
-	set startS (date +%s)
-	threeallel
-	set endS (date +%s)
-	echo "--------"
-	echo Started at:\t$start
-	echo Finished at:\t(date)
-	dc -e "$endS $startS - p"
-
+	dotime threeallel
 
 end
--- functions/timed-threeallel.fish
+++
@@ -1,4 +0,0 @@
-function timed-threeallel
-	ruby -e "require('benchmark');Benchmark.bm{|p|p.report{ IO.popen('fish -c threeallel'){|io|io.each{|b|STDOUT.write(b)}} }}"
-
-end
