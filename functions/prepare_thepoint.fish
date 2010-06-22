38e4d7857d64165ab22b36f9a3a18a5d56855630
toothrot <scissorjammer@gmail.com>
2010-06-22 10:22:56 -0500

    workworkwork

--- functions/prepare_thepoint.fish
+++ functions/prepare_thepoint.fish
@@ -1,7 +1,14 @@
 function prepare_thepoint
-	for i in (echo ""\n2\n3)
-echo db:test:load thepoint_test$i
-rake db:test:load TEST_ENV_NUMBER=$i
-end
+	
+	echo db:test:load thepoint_test
+	rake db:test:load TEST_ENV_NUMBER=&
+
+	echo db:test:load thepoint_test2
+	rake db:test:load TEST_ENV_NUMBER=2&
+
+	echo db:test:load thepoint_test3
+	rake db:test:load TEST_ENV_NUMBER=3&
+
+
 
 end
